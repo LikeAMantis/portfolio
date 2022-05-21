1a27fb9a5d30a689925aef83fbba6c5ef65e1183
David <d.guetl90@gmail.com>
2022-05-21 11:40:35 +0200

    sync

---
+++ components/Test.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const Test = () => {
+  return (
+    <div>Test</div>
+  )
+}
+
+export default Test
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        skin: {
+          fill: colors.neutral[800],
+          menu: colors.neutral[900],
+        },
+      },
+      textColor: {
+        skin: {
+          base: colors.neutral[200],
+          muted: colors.neutral[900],
+        },
+      },
+    },
   },
   plugins: [],
 }
